8048596117fe4cbff2bf9a4c156e5baf2919c873
VoronkoIllia <ilyhavoronko@gmail.com>
2024-09-10 11:07:22 +0300

    Виконано лаб 5

--- lab-5/task-2.py
+++ lab-5/task-2.py
@@ -1,4 +1,6 @@
-matrix = [[index if index<=7 else 0 for index in range(i,7+i)] for i in range(1,8)]
+n = 7
+
+matrix = [[index if index<=7 else 0 for index in range(i,n+i)] for i in range(1,n+1)]
 
 for i in range(len(matrix)):
     for j in range(len(matrix[i])):
